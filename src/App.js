66f1c0f76d4c076a20c0a69551fae40f956d6108
lukecheng1998 <lukecheng1998@gmail.com>
2020-06-28 20:37:30 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import './App.css';
+import "./App.css";
 import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
@@ -9,15 +9,17 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App margin-top">
+    <div className="App">
       <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={landing} />
-          <Route exact path="/login" component={login} />
-          <Route exact path="/signup" component={signup} />
-          <Route exact path="/home" component={home} />
-        </Switch>
+        <div className="container">
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={landing} />
+            <Route exact path="/login" component={login} />
+            <Route exact path="/signup" component={signup} />
+            <Route exact path="/home" component={home} />
+          </Switch>
+        </div>
       </Router>
     </div>
   );
