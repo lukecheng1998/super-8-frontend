70a70972ad8d807b1019cad3dd81f1455c728bf8
lukecheng1998 <lukecheng1998@gmail.com>
2020-06-28 22:10:31 -0700

    changed landing page

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
-
+import Typography from '@material-ui/core/Typography'
 export class landing extends Component {
     render() {
         return (
-            <h1>Landing Page</h1>
-        )
+            <div>
+                <Typography variant="h1">
+                    Super Team 8 Covid-19 Tracking website
+                </Typography>
+                <Typography variant="h2">
+                    Use the links to sign in to all of your services
+                </Typography>
+            </div>
+        )   
     }
 }
 
