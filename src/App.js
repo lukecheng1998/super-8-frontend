452063786e95c0068c46c87a91c58fae53dc172f
lukecheng1998 <lukecheng1998@gmail.com>
2020-06-30 23:52:51 -0700

    prevented automatic logout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import { Provider } from "react-redux";
+import jwtDecode from 'jwt-decode'
 //Web pages
 import home from "./pages/home";
 import login from "./pages/login";
@@ -14,7 +15,23 @@ import AuthRoute from './util/AuthRoute';
 //Redux elements
 import store from './redux/store';
 import axios from "axios";
+import { SET_AUTHENTICATED } from "./redux/types";
+import { getUserData, logoutUser } from "./redux/actions/userActions";
 axios.defaults.baseURL = "https://us-central1-super-8-1beb0.cloudfunctions.net/api"
+const token = localStorage.FBIdToken
+console.log(token);
+if(token){
+  const decodedToken = jwtDecode(token);
+  console.log(decodedToken);
+  if(decodedToken.exp * 1000 < Date.now()){
+    store.dispatch(logoutUser());
+    window.location.href = '/login'
+  }else{
+    store.dispatch({type: SET_AUTHENTICATED});
+    store.defaults.headers.common['Authorization'] = token;
+    store.dispatch(getUserData())
+  }
+}
 function App() {
   return (
     <div className="App">
