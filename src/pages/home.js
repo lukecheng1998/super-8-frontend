bf12a2bcd40de01edda10dede3a3634357b40b67
lukecheng1998 <lukecheng1998@gmail.com>
2020-07-10 13:59:39 -0700

    bluetooth popup

--- src/pages/home.js
+++ src/pages/home.js
@@ -10,7 +10,75 @@ import Button from "@material-ui/core/Button";
 const styles = (theme) => ({
   ...theme.spreadThis,
 });
+var connected = false;
+var selected_device;
+var connected_server;
 export class home extends Component {
+  discoverDevicesOrDisconnect = () => {
+    console.log("discoverDevicesOrDisconnect");
+    if (!connected) {
+      this.discoverDevices();
+    } else {
+      selected_device.gatt.disconnect();
+      this.resetUI();
+    }
+  };
+  discoverDevices = () => {
+    console.log("Discover Devices");
+    this.setConnectedStatus(false);
+    let options = {
+      acceptAllDevices: true,
+    };
+    navigator.bluetooth
+      .requestDevice(options)
+      .then((device) => {
+        console.log("> Name:             " + device.name);
+        console.log("> Id:               " + device.id);
+        console.log("> Connected:        " + device.gatt.connected);
+        selected_device = device;
+        console.log(selected_device);
+        this.connect();
+      })
+      .catch((error) => {
+        alert("ERROR: " + error);
+        console.log("ERROR: " + error);
+      });
+  };
+  connect = () => {
+    if (connected === false) {
+      console.log("connecting");
+      selected_device.gatt.connect().then(
+        function (server) {
+          console.log("Connected to " + server.device.id);
+          console.log("connected=" + server.connected);
+          this.setConnectedStatus(true);
+          connected_server = server;
+          selected_device.addEventListener(
+            "gattserverdisconnected",
+            this.onDisconnected()
+          );
+        },
+        function (error) {
+          console.log("ERROR: could not connect - " + error);
+          //Probably return a json
+          alert("Error: could not connect - " + error);
+          this.setConnectedStatus(false);
+        }
+      );
+    }
+  };
+  onDisconnected = () => {
+    console.log("onDisconnected");
+    this.resetUI();
+  };
+  setConnectedStatus = (status) => {
+    // We might not need this
+    connected = status;
+    return connected;
+  };
+  resetUI = () => {
+    this.setConnectedStatus(false);
+  };
   render() {
     const {
       classes,
@@ -22,23 +90,51 @@ export class home extends Component {
     } = this.props;
     return !loading ? (
       authenticated ? (
-        <div>
-          <Typography variant="h2" className={classes.pageTitle}>
-            Hi {handle}! Welcome to the dashboard Let's take a look at your
-            status.
-          </Typography>
-          <Button variant="contained" component={Link} to="/events">
-            events
-          </Button>
-          <Button
-                color="inherit"
+        <html>
+          <div>
+            <Typography variant="h2" className={classes.pageTitle}>
+              Hi {handle}! Welcome to the dashboard Let's take a look at your
+              status.
+            </Typography>
+            <Button variant="contained" component={Link} to="/events">
+              events
+            </Button>
+            <Button
+              color="inherit"
+              variant="contained"
+              component={Link}
+              to="/sickness"
+            >
+              Report Sickness
+            </Button>
+
+            <Typography variant="h3" className={classes.pageTitle}>
+              Web bluetooth
+            </Typography>
+            <Typography variant="h3" className={classes.pageTitle}>
+              status
+            </Typography>
+            {this.setConnectedStatus() ? (
+              <Button
                 variant="contained"
-                component={Link}
-                to="/sickness"
+                className={classes.button}
+                onClick={this.discoverDevicesOrDisconnect()}
+                id="btn-scan"
               >
-                Report Sickness
+                Discover Devices
               </Button>
-        </div>
+            ) : (
+              <Button
+                variant="contained"
+                className={classes.button}
+                onClick={this.discoverDevicesOrDisconnect()}
+                id="btn-scan"
+              >
+                Disconnect
+              </Button>
+            )}
+          </div>
+        </html>
       ) : (
         <p className={classes.pageTitle}>Please sign in to view this page!</p>
       )
